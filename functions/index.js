c05898eb504d04600e6a055774c0f0b86925f976
Yelmogus <yangli247@live.com>
2018-02-22 19:18:28 -0500

    Added Cloud Functions *Fixed*

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.createUser = functions.firestore
+  .document('users/{userId}')
+  .onCreate(event => {
+    // Get an object representing the document
+    // e.g. {'name': 'Marie', 'age': 66}
+    var newValue = event.data.data();
+
+    // access a particular field as you would any JS property
+    var name = newValue.name;
+
+    // perform desired operations ...
+});
