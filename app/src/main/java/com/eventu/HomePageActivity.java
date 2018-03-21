366e6dddd4e02e7ffb3563688d6b7229f75ad6dd
yuiop97 <duj@rpi.edu>
2018-03-21 17:03:07 -0400

    added club page and edit club page club page still needs to retrieve data to display club page still needs to retrieve school name from homepage in event_card_view.xml event creator textview is now clickable, still needs to launch DisplayClubPage activity on click

--- app/src/main/java/com/eventu/HomePageActivity.java
+++ app/src/main/java/com/eventu/HomePageActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
@@ -246,4 +247,5 @@ public class HomePageActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
     }
+
 }
