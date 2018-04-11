48dfbb18df2a7b4772794da49fc92551e76e37a7
Joe Grogan <grogaj@rpi.edu>
2018-04-11 13:20:51 -0400

    Starting work on image selection. Allows user to select an image from their phone's gallery but doesn't do anything with it yet.

--- app/src/main/java/com/eventu/CreateEventActivity.java
+++ app/src/main/java/com/eventu/CreateEventActivity.java
@@ -1,11 +1,15 @@
 package com.eventu;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
@@ -21,6 +25,8 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -40,6 +46,8 @@ public class CreateEventActivity extends BaseClass {
     private EditText mEventDescription;
     private TimePicker mTimePicker;
     private DatePicker mDatePicker;
+    private Button mImageButton;
+    private Bitmap bmp;
 
     // Database References
     private CollectionReference mSchoolClubEvents;
@@ -53,6 +61,8 @@ public class CreateEventActivity extends BaseClass {
     private String ClubID = "";
     private String eventID = "";
 
+    private int RESULT_LOAD_IMAGE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +75,7 @@ public class CreateEventActivity extends BaseClass {
         mTimePicker = findViewById(R.id.tp_timepicker);
         mDatePicker = findViewById(R.id.dp_datepicker);
         mDatePicker.setMinDate(System.currentTimeMillis());
+        mImageButton = findViewById(R.id.select_image_button);
         FloatingActionButton nextButton = findViewById(R.id.next_button);
 
         if (mCurrentUser == null) {
@@ -81,6 +92,42 @@ public class CreateEventActivity extends BaseClass {
                 attemptAddEvent();
             }
         });
+
+        // On click of image button, open new activity to select an image from the phone's gallery
+        mImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                    photoPickerIntent.setType("image/*");
+                    startActivityForResult(photoPickerIntent, RESULT_LOAD_IMAGE);
+                } catch (Exception e) {
+                    Log.i("Error", e.getMessage());
+                }
+            }
+        });
+
+    }
+
+    /**
+     * Reads image as a bitmap when image selection activity is finished.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+
+            try {
+                final InputStream imageStream = getContentResolver().openInputStream(selectedImage);
+                bmp = BitmapFactory.decodeStream(imageStream);
+            } catch (IOException e) {
+                Log.i("Error", e.getMessage());
+            }
+        } else {
+            Toast.makeText(CreateEventActivity.this, "You haven't picked Image",
+                    Toast.LENGTH_LONG).show();
+        }
 
     }
 
