5aa83c681dd454ac190e47f6641536ad0e9890fd
Joe Grogan <grogaj@rpi.edu>
2018-04-16 21:52:27 -0400

    Added Espresso-Intent and Mockito library support. Added Instrumentation Tests for the StartPageActivity

---
+++ app/src/androidTest/java/com/eventu/StartPage.java
@@ -0,0 +1,71 @@
+package com.eventu;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.rule.ActivityTestRule;
+
+import com.eventu.login_and_registration.LoginActivity;
+import com.eventu.login_and_registration.StartPageActivity;
+import com.eventu.login_and_registration.school_selection.SchoolSelectActivity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class StartPage {
+
+    @Rule
+    public final ActivityTestRule<StartPageActivity> mActivityRule = new ActivityTestRule<>(
+            StartPageActivity.class);
+
+    @Before
+    public void before() {
+        Intents.init();
+    }
+
+    @Test
+    public void registerButton() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        SharedPreferences prefs = context.getSharedPreferences(
+                context.getString(R.string.USER_PREFS_FILE), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean(context.getString(R.string.RememberAccess), false);
+        editor.commit();
+
+        mActivityRule.launchActivity(new Intent());
+        intended(hasComponent(StartPageActivity.class.getName()));
+        onView(withId(R.id.register_button)).perform(click());
+        intended(hasComponent(SchoolSelectActivity.class.getName()));
+    }
+
+    @Test
+    public void logInButton() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        SharedPreferences prefs = context.getSharedPreferences(
+                context.getString(R.string.USER_PREFS_FILE), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean(context.getString(R.string.RememberAccess), false);
+        editor.commit();
+
+        mActivityRule.launchActivity(new Intent());
+        intended(hasComponent(StartPageActivity.class.getName()));
+        onView(withId(R.id.log_in_button)).perform(click());
+        intended(hasComponent(LoginActivity.class.getName()));
+    }
+
+    @After
+    public void after() {
+        mActivityRule.finishActivity();
+        Intents.release();
+    }
+}
